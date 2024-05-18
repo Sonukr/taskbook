b932ef1a2e459f328ebea4eb1ea92cda8f935f5b
Sonu <Sonukr@users.noreply.github.com>
2024-05-18 12:21:27 +0530

    remove task from dist

--- backend/dist/tasks/dto/create-task.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateTaskDto {
-    name: String;
-    status: String;
-}
--- backend/dist/tasks/schemas/tasks.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskSchema = void 0;
-const mongoose = require("mongoose");
-exports.TaskSchema = new mongoose.Schema({
-    name: String,
-    status: String,
-});
-//# sourceMappingURL=tasks.schema.js.map
